3e6a9b270e26f2f530b9428dbf466dee630a0d08
ENvironmentSet <sbshw1@gmail.com>
2019-02-02 17:22:41 +0900

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: resolve('src', 'app'),
+  output: {
+    filename: 'bundle.js',
+    path: resolve('dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    hot: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Cyclejs todo application',
+    }),
+    new HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              camelCase: true,
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
