e3abac4c404c6add04ebce934f36e32f79fbddb0
ENvironmentSet <sbshw1@gmail.com>
2019-02-08 02:56:35 +0900

    disable local scoped css

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,12 @@ module.exports = {
         test: /\.css$/,
         use: [
           { loader: 'style-loader' },
-          { loader: 'css-loader' }
+          {
+            loader: 'css-loader',
+            options: {
+              modules: false,
+            }
+          }
         ]
       }
     ]
