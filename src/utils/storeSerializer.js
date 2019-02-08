4fa19f14b7d065dce7f475276d32594d3eb2bb26
ENvironmentSet <sbshw1@gmail.com>
2019-02-08 23:30:14 +0900

    fix wrong function

--- src/utils/storeSerializer.js
+++ src/utils/storeSerializer.js
@@ -1,4 +1,4 @@
-import { replace, pipe, mapObjIndexed, ifElse, is, identity, compose, equals, flip, constructN, curryN } from 'ramda';
+import { replace, pipe, mapObjIndexed, ifElse, is, identity, compose, equals, flip, constructN, nAry } from 'ramda';
 
 export const storeSerializer = pipe(
   mapObjIndexed(ifElse(is(RegExp), compose(replace(/\//g, ''), String), identity)),
@@ -6,6 +6,6 @@ export const storeSerializer = pipe(
 );
 
 export const storeDeserializer = pipe(
-  curryN(1, JSON.parse),
+  nAry(1, JSON.parse),
   mapObjIndexed(ifElse(flip(equals('todoFilter')), constructN(1, RegExp), identity)),
 );
