c303a0b48f6e0b679738362559e9a32fae594abe
ENvironmentSet <sbshw1@gmail.com>
2019-02-08 23:53:18 +0900

    replace empty string to default va

--- src/store/ducks/todoFilter.js
+++ src/store/ducks/todoFilter.js
@@ -1,5 +1,5 @@
 import { createActions, handleActions } from 'redux-actions';
-import { constructN, tryCatch } from 'ramda';
+import { constructN, tryCatch, ifElse, compose, isEmpty, identity } from 'ramda';
 
 import { pickPayload } from '^/utils';
 
@@ -9,7 +9,7 @@ const SET_TODO_FILTER = 'SET_TODO_FILTER';
 const CLEAR_TODO_FILTER = 'CLEAR_TODO_FILTER';
 
 export const { setTodoFilter, clearTodoFilter } = createActions({
-  [SET_TODO_FILTER]: tryCatch(constructN(1, RegExp), createDefaultTodoFilter)
+  [SET_TODO_FILTER]: compose(tryCatch(constructN(1, RegExp), createDefaultTodoFilter), ifElse(isEmpty, createDefaultTodoFilter, identity))
 }, CLEAR_TODO_FILTER);
 
 export default handleActions({
